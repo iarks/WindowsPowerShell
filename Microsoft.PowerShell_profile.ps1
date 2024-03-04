43e865dbd2e52411f5c2172dff0c3543565b3099
arkadeep saha <arkadeepsaha11@gmail.com>
2024-03-04 23:51:39 +0530

    initial-commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+# oh-my-posh init pwsh | Invoke-Expression
+$ENV:STARSHIP_CONFIG = "C:\Users\arkad\Documents\WindowsPowerShell\starship-presets\gruvbox-rainbow_ark.toml"
+Invoke-Expression (&starship init powershell)
